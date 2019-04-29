c2503f02bb05b88d1fe9c07c1f4bfbfb8e5fe317
v-zhumin <v-zhumin@DESKTOP-KNMM69A>
2019-04-29 16:41:09 +0800

    Add project files.

---
+++ App14/App14/IFileLauncher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App14
+{
+    public interface IFileLauncher
+    {
+        Task<bool> LaunchFileAsync(string uri);
+    }
+}
---
+++ App14/App14/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+
+namespace App14
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+          
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+             await Browser.OpenAsync(new Uri("file:///C:/Users/v-zhumin/Desktop/hello.txt"), BrowserLaunchMode.External);
+      //   var ss =   await DependencyService.Get<IFileLauncher>().LaunchFileAsync("D:\\Key.pdf");
+        }
+    }
+}
